f2e807f2d5ef80d218973c2772d394fbae1fab7e
Justin Tulloss <justin@rd.io>
2010-10-18 17:26:30 -0700

    version 1 of api docs

---
+++ js-api/javascripts/pdoc/item_index.js
@@ -0,0 +1,22 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'Bujagali.Monad#exec': { 'name': 'Bujagali.Monad#exec', 'type': 'instance method', 'path': 'Bujagali/Monad/prototype/exec/index.html' },
+'Bujagali.Monad': { 'name': 'Bujagali.Monad', 'type': 'class', 'path': 'Bujagali/Monad/index.html' },
+'Bujagali': { 'name': 'Bujagali', 'type': 'namespace', 'path': 'Bujagali/index.html' },
+'Bujagali.Utils.escape': { 'name': 'Bujagali.Utils.escape', 'type': 'class method', 'path': 'Bujagali/Utils/escape/index.html' },
+'Bujagali.Utils.date': { 'name': 'Bujagali.Utils.date', 'type': 'class method', 'path': 'Bujagali/Utils/date/index.html' },
+'Bujagali.Utils': { 'name': 'Bujagali.Utils', 'type': 'namespace', 'path': 'Bujagali/Utils/index.html' },
+'Bujagali.render': { 'name': 'Bujagali.render', 'type': 'class method', 'path': 'Bujagali/render/index.html' },
+'Bujagali.postProcessors': { 'name': 'Bujagali.postProcessors', 'type': 'class property', 'path': 'Bujagali/postProcessors/index.html' },
+'Bujagali.Utils.deEscape': { 'name': 'Bujagali.Utils.deEscape', 'type': 'class method', 'path': 'Bujagali/Utils/deEscape/index.html' },
+'new Bujagali.Monad': { 'name': 'new Bujagali.Monad', 'type': 'constructor', 'path': 'Bujagali/Monad/new/index.html' },
+'Bujagali.Utils.urlize': { 'name': 'Bujagali.Utils.urlize', 'type': 'class method', 'path': 'Bujagali/Utils/urlize/index.html' },
+'Bujagali.Utils.enrich': { 'name': 'Bujagali.Utils.enrich', 'type': 'class method', 'path': 'Bujagali/Utils/enrich/index.html' },
+'Bujagali.Monad#load': { 'name': 'Bujagali.Monad#load', 'type': 'instance method', 'path': 'Bujagali/Monad/prototype/load/index.html' },
+'Bujagali.Monad#render': { 'name': 'Bujagali.Monad#render', 'type': 'instance method', 'path': 'Bujagali/Monad/prototype/render/index.html' },
+'Bujagali.mixin': { 'name': 'Bujagali.mixin', 'type': 'class method', 'path': 'Bujagali/mixin/index.html' },
+'Bujagali.Monad#afterRender': { 'name': 'Bujagali.Monad#afterRender', 'type': 'instance method', 'path': 'Bujagali/Monad/prototype/afterRender/index.html' },
+'Bujagali.Utils.randomID': { 'name': 'Bujagali.Utils.randomID', 'type': 'class method', 'path': 'Bujagali/Utils/randomID/index.html' },
+'Bujagali.renderMacro': { 'name': 'Bujagali.renderMacro', 'type': 'class method', 'path': 'Bujagali/renderMacro/index.html' },
+'Bujagali.Utils.truncate': { 'name': 'Bujagali.Utils.truncate', 'type': 'class method', 'path': 'Bujagali/Utils/truncate/index.html' }
+};
